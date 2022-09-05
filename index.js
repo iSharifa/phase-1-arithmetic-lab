cd610deec2ab9a240d9bec32a97785ffb32120c0
Sharifa Islam <sislam6@buffalo.edu>
2022-09-04 21:40:51 -0400

    commit tree 47efb6888fd068da0972fdae8cb6aca92cf11fd7

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+   let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+const num3 = 22;
+const num4 = 6;
+    let mod= num3 % num4;
+
+ const max = Math.max(1,5,9,13,20)
+ 
+
